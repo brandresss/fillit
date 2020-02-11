52606e8cc45109979952ef8f8d292ebd8c41cf81
Brandi Andres <brandres@at-m2.21-school.ru>
2020-02-11 22:24:16 +0300

    g

--- f/map.c
+++ f/map.c
@@ -7,14 +7,39 @@ char **map(int size)
     char **maps;
     maps = (char **)malloc(sizeof(char *) * (size + 1));
     while (i < 4)
+    {
         maps[i++] = (char *)malloc(sizeof(char) * (size + 1));
+        ft_memset(maps[i], ".", size + 1);
+    }
     return(maps);
 }
 
-void algo(char **maps, t_list *element)
+int suchka(t_list *tetrim, char **maps, int x, int y) {}
+void get_full(t_list *tetrim, char **maps) {}
+
+void recurs(char **maps, t_list *tetrim, int size)
 {
+    int x;
+    int y;
+    x = 0;
+    while(tetrim)
+    {
+        if (suchka(tetrim, maps, x, y))
+        {
+            get_full(tetrim, maps);
+            tetrim = tetrim->next;
+        }
+        if (x < size)
+            x++;
+        else if (y < size)
+        {
+            y++;
+            x = 0;
+        }
+        else if (x == size && y == size)
+
+    }
+        
+    }
 
 }
-
-
-int exist()
