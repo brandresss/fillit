5caebcece02e0a8a4119745060eef0b1d889481b
Brandi Andres <brandres@at-n7.21-school.ru>
2020-02-20 18:10:33 +0300

    g

--- f/main.c
+++ f/main.c
@@ -6,221 +6,182 @@
 /*   By: brandres <brandres@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/18 22:11:09 by brandres          #+#    #+#             */
-/*   Updated: 2020/02/19 23:21:55 by brandres         ###   ########.fr       */
+/*   Updated: 2020/02/20 17:56:09 by brandres         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/fillit.h"
 
-void    ft_error(void)
+void	ft_error(void)
 {
-    ft_putstr("error\n");
-    exit(EXIT_FAILURE);
+	ft_putstr("error\n");
+	exit(EXIT_FAILURE);
 }
 
-char    max_y(char *koord)
+char	max_y(char *koord)
 {
-    int     i;
-    char     tmp;
+	int		i;
+	char	tmp;
 
-    i = 0;
-    while(i <= 6)
-    {
-        if(tmp < koord[i])
-            tmp = koord[i];
-        i += 2;
-    }
-    return(tmp);
+	i = 0;
+	while (i <= 6)
+	{
+		if (tmp < koord[i])
+			tmp = koord[i];
+		i += 2;
+	}
+	return (tmp);
 }
 
-char    max_x(char *koord)
+char	max_x(char *koord)
 {
-    int     j;
-    char     tmp;
+	int		j;
+	char	tmp;
 
-    j = 1;
-    while(j <= 7)
-    {
-        if(tmp < koord[j])
-            tmp = koord[j];
-        j += 2;
-    }
-    return(tmp);
+	j = 1;
+	while (j <= 7)
+	{
+		if (tmp < koord[j])
+			tmp = koord[j];
+		j += 2;
+	}
+	return (tmp);
 }
 
-char    *koordin(char *buf)
+char	*koordin(char *buf)
 {
-    int i;
-    int j;
-    char *koord;
+	int		i;
+	int		j;
+	char	*koord;
 
-    koord = (char *)malloc(sizeof(char) * 9);
-    koord[8] = '\0';
-    i = 0;
-    j = 0;
-    //printf("buff:%s\n", buf);
-    while(i <= 19)
-    {
-        if(buf[i] == '#')
-        {
-            koord[j] = i / 5 + '0';
-
-            koord[j + 1] = i % 5 + '0';
-            j += 2;
-        }
-        i++;
-    }
-    //printf("%s\n", koord);
-    return(koord);
+	koord = (char *)malloc(sizeof(char) * 9);
+	koord[8] = '\0';
+	i = 0;
+	j = 0;
+	while (i <= 19)
+	{
+		if (buf[i] == '#')
+		{
+			koord[j] = i / 5 + '0';
+			koord[j + 1] = i % 5 + '0';
+			j += 2;
+		}
+		i++;
+	}
+	//printf("%s\n", koord);
+	return (koord);
 }
 
-char *null_pos(char *koord)
+char	*null_pos(char *koord)
 {
-    int     i;
-    int     c;
-    int     j;
-    int     tmp;
-    i = 0;
-    j = 1;
-    c = 3;
-    while(i <= 6)
-    {
-        if(c > koord[i] - '0')
-            c = koord[i] - '0';
-        i += 2;
-    }
-    tmp = 3;
-    while(j <= 7)
-    {
-        if(tmp > koord[j] - '0')
-            tmp = koord[j] - '0';
-        j += 2;
-    }
-    //printf("c tmp:%d %d\n", c, tmp);
-    j = 1;
-    while(j <= 7)
-    {
-        koord[j] =  koord[j] - tmp;
-        j += 2;
-    }
-    i = 0;
-    while(i <= 6)
-    {
-        koord[i] =  koord[i] - c;
-        i += 2;
-    }
-    //printf("koord:%s\n", koord);
-    return(koord);
+	int		i;
+	int		c;
+	int		j;
+	int		tmp;
+
+	i = 0;
+	j = 1;
+	c = 3;
+	while (i <= 6)
+	{
+		if (c > koord[i] - '0')
+			c = koord[i] - '0';
+		i += 2;
+	}
+	tmp = 3;
+	while (j <= 7)
+	{
+		if (tmp > koord[j] - '0')
+			tmp = koord[j] - '0';
+		j += 2;
+	}
+	//printf("c tmp:%d %d\n", c, tmp);
+	j = 1;
+	while (j <= 7)
+	{
+		koord[j] = koord[j] - tmp;
+		j += 2;
+	}
+	i = 0;
+	while (i <= 6)
+	{
+		koord[i] = koord[i] - c;
+		i += 2;
+	}
+	return (koord);
 }
 
-t_list   *reader(const int fd)
+t_list	*reader(const int fd)
 {
-    char    *buf;
-    int     save;
-    char    letter;
-    t_list  *list;
+	char	*buf;
+	int		save;
+	char	letter;
+	t_list	*list;
+	int		save1;
 
-    save = 0;
-    buf = ft_strnew(21);
-    letter = 'A';
-    list = NULL;
-    while ((save = read(fd, buf, 21)))
-    {
-        // если прочитано не 20 и не 21 символ, то ошибка (наверно?)
-        if (save != 20 && save != 21)
-            ft_error();
-        if ((checker(save, buf)) != 5)
-        {
-            ft_strdel(&buf);
-            ft_error();
-        }
-        if (list == NULL)
-            list = create_tetrimino(null_pos(koordin(buf)), \
-            max_y(null_pos(koordin(buf))), max_x(null_pos(koordin(buf))), letter++);
-        // если уже есть, то добавляем элемент в конец
-        else
-            ft_lstadd_end(&list, create_tetrimino(null_pos(koordin(buf)), \
-            max_y(null_pos(koordin(buf))), max_x(null_pos(koordin(buf))), letter++));
-
-    }
-    //printf("list->content%s\n", list->content);
-    return(list);
+	save1 = 0;
+	buf = ft_strnew(21);
+	letter = 'A';
+	list = NULL;
+	while ((save = read(fd, buf, 21)))
+	{
+	// если прочитано не 20 и не 21 символ, то ошибка (наверно?)
+		if (save != 20 && save != 21)
+			ft_error();
+		if ((checker(save, buf)) != 5)
+		{
+			ft_strdel(&buf);
+			ft_error();
+		}
+		if (list == NULL)
+			list = create_tetrimino(null_pos(koordin(buf)), \
+			max_y(null_pos(koordin(buf))), max_x(null_pos(koordin(buf))), \
+			letter++);
+		else
+			ft_lstadd_end(&list, create_tetrimino(null_pos(koordin(buf)), \
+			max_y(null_pos(koordin(buf))), max_x(null_pos(koordin(buf))), \
+			letter++));
+		save1 = save;
+	}
+	if ((save == 0 && save1 == 21) || (save1 == 0 && save == 0))
+		ft_error();
+	return (list);
 }
 
-/*void    frree(char **maps, int size)
+void	kabel(t_list *tetrim)
 {
-    int i;
+	char	**maps;
+	int		size;
 
-    i = 0;
-    //printf("size:%d\n", size);
-
-    while(maps[i] != NULL)
-    {
-        free(*maps);
-        maps++;
-
-
-        i++;
-    }
-
-
-    //free(*maps);
-}*/
-
-void    kabel(t_list *tetrim)
-{
-    char **maps;
-    int     size;
-
-    size = 2;
-    maps = map(size);
-    //printf("4\n");
-    //printf("!str:%s!\n", tetrim->content);
-    while (recurs(maps, tetrim, size) == 2)
-    {
-
-
-        //frree(maps, size);
-        free(maps);
-        size++;
-
-        //printf("maps after free\n");
-        //print_map(maps, size);
-        //printf("maps after free end\n");
-        maps = map(size);
-    }
-    printf("\n");
-    print_map(maps, size);
-    //printf("5\n");
+	size = 2;
+	maps = map(size);
+	while (recurs(maps, tetrim, size) == 2)
+	{
+		free(maps);
+		size++;
+		maps = map(size);
+	}
+	printf("\n");
+	print_map(maps, size);
 }
 
-
-int     main (int argc, char **argv)
+int		main(int argc, char **argv)
 {
-     //printf("0\n");
-    // ft_putchar('!');
-    int fd;
-    t_list *list;
+	int		fd;
+	t_list	*list;
 
-
-    if (argc != 2)
-    {
-        ft_putstr("usage: ./fillit source_file\n");
-        exit(EXIT_FAILURE);
-    }
-
-    fd = open(argv[1], O_RDONLY);
-    if (fd == -1)
-    {
-        ft_error();
-    }
-   // printf("1\n");
-    list = reader(fd);
-    close(fd);
-    //printf("2\n");
-    //printf("%s\n", list->content);
-   // printf("3\n");
-    kabel(list);
-
-    return (0);
+	if (argc != 2)
+	{
+		ft_putstr("usage: ./fillit source_file\n");
+		exit(EXIT_FAILURE);
+	}
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		ft_error();
+	}
+	list = reader(fd);
+	close(fd);
+	kabel(list);
+	return (0);
 }
--- f/map.c
+++ f/map.c
@@ -6,16 +6,16 @@
 /*   By: brandres <brandres@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/15 16:36:58 by brandres          #+#    #+#             */
-/*   Updated: 2020/02/19 23:09:07 by brandres         ###   ########.fr       */
+/*   Updated: 2020/02/20 18:00:53 by brandres         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/fillit.h"
 
-char    **map(int size)
+char	**map(int size)
 {
-    int i;
-    char **maps;
+	int		i;
+	char	**maps;
 
     i = 0;
     maps = (char **)malloc(sizeof(char *) * (size + 1));
@@ -27,153 +27,120 @@ char    **map(int size)
         i++;
     }
     maps[i] = NULL;
-    return(maps);
+    return (maps);
 }
 
-
-/*int     suchka(t_list *tetrim, char **maps, int x, int y, int size)
+int		suchka(t_list *tetrim, char **maps, int x, int y, int  size)
 {
-    int i;
-    int j;
+	int		i;
+	int		j;
 
-    i = 0;
-    j = 1;
-    while (i <= 6 && j <= 7)
-    {
-        //printf("%d\n", tetrim->content[i] - '0' + x);
-
-        if ((tetrim->content[i] - '0' + y) >= size || \
-        (tetrim->content[j] - '0' + x) >= size)
-            return (2);
-        if (maps[tetrim->content[i] - '0' + y][tetrim->content[j] - '0' + x] != '.' && x <= )
-            return(0);
-        i = i + 2;
-        j = j + 2;
-
-    }
-    return(1);
-}*/
-
-int     suchka(t_list *tetrim, char **maps, int x, int y, int  size)
-{
-    int i;
-    int j;
-
-    i = 0;
-    j = 1;
-    if((tetrim->max_y + y) > size - 1 + '0')
-        return(2);
-    if((tetrim->max_x + x) > size - 1 + '0')
-        return(0);
-    while (i <= 6 && j <= 7)
-    {
-        if (maps[tetrim->content[i] - '0' + y][tetrim->content[j] - '0' + x] != '.')
-            return(0);
-        i += 2;
-        j += 2;
-    }
-    return(1);
+	i = 0;
+	j = 1;
+	if ((tetrim->max_y + y) > size - 1 + '0')
+		return (2);
+	if ((tetrim->max_x + x) > size - 1 + '0')
+		return (0);
+	while (i <= 6 && j <= 7)
+	{
+		if (maps[tetrim->content[i] - '0' + y][tetrim->content[j] - '0' + x] != '.')
+			return (0);
+		i += 2;
+		j += 2;
+	}
+	return (1);
 }
 
-void    get_full(t_list *tetrim, char **maps, int x, int y)
+void	get_full(t_list *tetrim, char **maps, int x, int y)
 {
-    int i;
-    int j;
+	int		i;
+	int		j;
 
-    i = 0;
-    j = 1;
-    while (i <= 6 && j <= 7)
-    {
-        maps[tetrim->content[i] - '0' + y][tetrim->content[j] - '0' + x] = tetrim->letter;
-        i += 2;
-        j += 2;
-    }
+	i = 0;
+	j = 1;
+	while (i <= 6 && j <= 7)
+	{
+		maps[tetrim->content[i] - '0' + y][tetrim->content[j] - '0' + x] = tetrim->letter;
+		i += 2;
+		j += 2;
+	}
 
 }
 
-void    del_let(char **maps, char let)
+void	del_let(char **maps, char let)
 {
-    int i;
-    int j;
+	int		i;
+	int		j;
 
-    i = 0;
-    j = 0;
-    while(maps[j] != NULL)
-    {
-        i = 0;
-        while(maps[j][i] != '\0')
-        {
-            if(maps[j][i] == let)
-                maps[j][i] = '.';
-            i++;
-        }
-        j++;
-    }
+	i = 0;
+	j = 0;
+	while (maps[j] != NULL)
+	{
+		i = 0;
+		while (maps[j][i] != '\0')
+		{
+			if (maps[j][i] == let)
+				maps[j][i] = '.';
+			i++;
+		}
+		j++;
+	}
 }
 
-int     recurs(char **maps, t_list *tetrim, int size)
+int		recurs(char **maps, t_list *tetrim, int size)
 {
-    int x;
-    int y;
-    int save;
-    x = 0;
-    y = 0;
-    save = 0;
+	int		x;
+	int		y;
+	int		save;
 
-    while(x <= size && y <= size)
-    {
-        if ((save = suchka(tetrim, maps, x, y, size)) == 1)
-        {
-            get_full(tetrim, maps, x, y);
-           // print_map(maps, size);
-            //tetrim = tetrim->next;
-            if (tetrim->next)
-            {
-                if(recurs(maps, tetrim->next, size) == 9)
-                    return (9);
-                else
-                {
-                    del_let(maps, tetrim->letter);
-                    save = 0;
-                }
-                //return(2);
-            }
-            else
-                return(9);
-        }
-        if (save != 1)
-        {
-            //if (suchka(tetrim, maps, x, y, size) == 2)
-            if (save == 2)
-            {
-                //del_let(maps,tetrim->letter);
-                //printf("after del\n%s\n", maps[0]);
-                return(2);
-            }
-            if (x < size)
-                x++;
-            else if (y < size)
-            {
-                y++;
-                x = 0;
-            }
-        }
-    }
-    //else if (x == size && y == size)
-    return (0);
-
+	x = 0;
+	y = 0;
+	save = 0;
+	while (x <= size && y <= size)
+	{
+		if ((save = suchka(tetrim, maps, x, y, size)) == 1)
+		{
+			get_full(tetrim, maps, x, y);
+			if (tetrim->next)
+			{
+				if (recurs(maps, tetrim->next, size) == 9)
+					return (9);
+				else
+				{
+					del_let(maps, tetrim->letter);
+					save = 0;
+				}
+			}
+			else
+				return (9);
+		}
+		if (save != 1)
+		{
+			if (save == 2)
+			{
+				return (2);
+			}
+			if (x < size)
+				x++;
+			else if (y < size)
+			{
+				y++;
+				x = 0;
+			}
+		}
+	}
+	return (0);
 }
 
-void    print_map(char **map, int size)
+void	print_map(char **map, int size)
 {
-    int i;
-    i = 0;
+	int		i;
 
-    //printf(":%s:\n", map[0]);
-    while(i < size)
-    {
-        ft_putstr(map[i]);
-        ft_putchar('\n');
-        i++;
-    }
+	i = 0;
+	while (i < size)
+	{
+		ft_putstr(map[i]);
+		ft_putchar('\n');
+		i++;
+	}
 }
